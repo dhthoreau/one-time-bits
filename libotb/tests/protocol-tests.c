eda3a6aaf51dd4655b626284f25f901b5c2062fa
dhthoreau <dhthoreau@server.fake>
2015-12-06 14:26:40 -0500

    2014 => 2015

--- libotb/tests/protocol-tests.c
+++ libotb/tests/protocol-tests.c
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2014 the OTB team
+ * Copyright © 2015 the OTB team
  * 
  * This work is free. You can redistribute it and/or modify it under the
  * terms of the Do What The Fuck You Want To Public License, Version 2,
