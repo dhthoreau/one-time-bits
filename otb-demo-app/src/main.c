4669a90fbfb7e3bfd39e7384bfeb04f4797e6d4e
dhthoreau <dhthoreau@server.fake>
2017-01-22 11:34:54 -0500

    Cancellare codice non usata (main.c).

--- otb-demo-app/src/main.c
+++ otb-demo-app/src/main.c
@@ -20,43 +20,17 @@
 
 #include "main.h"
 
-static void destroy_bitkeeper(OtbBitkeeper *bitkeeper)
-{
-	otb_bitkeeper_shutdown_tasks(bitkeeper);
-	g_object_unref(bitkeeper);
-}
-
-static void run_otb_demo_app_window(OtbBitkeeper *bitkeeper)
-{
-	otb_bitkeeper_launch_tasks(bitkeeper);
-	const GtkWindow *window=NULL;
-	otb_demo_app_create_window("main.ui", NULL, NULL);
-	g_signal_connect_after(GTK_WIDGET(window), "delete_event", G_CALLBACK(destroy_bitkeeper), bitkeeper);
-	gtk_widget_show(GTK_WIDGET(window));
-	gtk_main();
-	gtk_widget_destroy(GTK_WIDGET(window));
-}
-
 #define otb_data_is_corrupted_or_missing()	(!otb_local_crypto_can_be_unlocked() || !otb_bitkeeper_exists() || !otb_user_exists())
 
 static void activate(GtkApplication *application, const void *user_data)
 {
 	otb_settings_initialize(OTB_DEMO_APP_NAME, "otb");
-	const OtbBitkeeper *bitkeeper=NULL;
 	if(otb_data_is_corrupted_or_missing())
 		otb_demo_create_user_show_new_window(application);
 	else if(otb_local_crypto_unlock_sym_cipher(""))
 		otb_demo_console_show_new_window(application);
 	else
 		otb_demo_passphrase_unlock_show_new_window(application);
-/*
-{
-		bitkeeper=otb_bitkeeper_load();
-		if(bitkeeper==NULL)
-			g_error(_("Failed to load bitkeeper."));
-		run_otb_demo_app_window(bitkeeper);
-}
-*/
 }
 
 int main(int argc, char *argv[])
